d324f1c9d681ff4287f41851fc9ab472483d19b1
1028473 - Mees Muller <89028475+TheMazeIsAmazing@users.noreply.github.com>
2022-06-22 10:34:13 +0200

    Update button.ts

--- src/button.ts
+++ src/button.ts
@@ -1,15 +1,28 @@
+//import pixi
 import * as PIXI from 'pixi.js'
 
+//this class is used on the character customisation screen
 export class button extends PIXI.Sprite {
 
-    constructor(game: PIXI.Application, texture : PIXI.Texture) {
+    constructor(game: PIXI.Application, texture : PIXI.Texture, leftOrRight : string) {
         super(texture)
         this.anchor.set(1, 0.5)
-        this.scale.set(0.8)
-        this.x = game.view.width
-        this.y = game.view.height / 2
-        game.stage.addChild(this)
-    }
+        this.interactive = true
+        this.buttonMode = true
+        
+        if (leftOrRight == 'right') {
+            this.x = game.view.width
+            this.y = game.view.height / 2
 
-    
+        } else {
+            this.x = 0
+            this.y = game.view.height / 2
+
+            //this code mirrors the texture
+            this.scale.x *= -1
+        }
+
+        //add the instance to the gamw
+        game.stage.addChild(this)
+    }  
 }
